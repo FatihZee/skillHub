92d0ae183bd03868d9e85983d7bbd82bbc1552a1
FATIH FIKRY OKTAVIANTO <viananto1234@gmail.com>
2024-09-15 12:06:02 +0700

    15 Sep 2024

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const userRoutes = require('./routes/userRoutes');
-const portfolioRoutes = require('./routes/portfolioRoutes');
-const skillRoutes = require('./routes/skillRoutes');
-const userSkillRoutes = require('./routes/userSkillRoutes');
-const authRoutes = require('./routes/authRoutes');
-const serviceRoutes = require('./routes/serviceRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const midtransRoutes = require('./routes/midtransRoutes');
-const RatingRoutes = require('./routes/RatingRoutes');
-const bankAccountRoutes = require('./routes/bankAccountRoutes');
-const bankRoutes = require('./routes/bankRoutes');
-const skillSwapRoutes = require('./routes/skillSwapRoutes');
-const waRoutes = require('./routes/waRoutes');
-require('dotenv').config();
+const path = require("path");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const userRoutes = require("./routes/userRoutes");
+const portfolioRoutes = require("./routes/portfolioRoutes");
+const skillRoutes = require("./routes/skillRoutes");
+const userSkillRoutes = require("./routes/userSkillRoutes");
+const authRoutes = require("./routes/authRoutes");
+const serviceRoutes = require("./routes/serviceRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const midtransRoutes = require("./routes/midtransRoutes");
+const RatingRoutes = require("./routes/RatingRoutes");
+const bankAccountRoutes = require("./routes/bankAccountRoutes");
+const bankRoutes = require("./routes/bankRoutes");
+const skillSwapRoutes = require("./routes/skillSwapRoutes");
+const waRoutes = require("./routes/waRoutes");
+require("dotenv").config();
 
 // Setup Swagger options
 const swaggerOptions = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'API Documentation for SkillHub Application',
-      version: '1.0.0',
-      description: 'Dokumentasi untuk endpoint API dari aplikasi SkillHub. Aplikasi ini mencakup berbagai fitur, termasuk manajemen pengguna, portofolio, keterampilan, layanan, dan pesanan. Dokumentasi ini bertujuan untuk mempermudah pengembang dalam memahami dan menggunakan API yang disediakan.',
+      title: "API Documentation for SkillHub Application",
+      version: "1.0.0",
+      description: `Dokumentasi untuk endpoint API dari aplikasi SkillHub. Aplikasi ini mencakup berbagai fitur, termasuk manajemen pengguna, portofolio, keterampilan, layanan, dan pesanan. Dokumentasi ini bertujuan untuk mempermudah pengembang dalam memahami dan menggunakan API yang disediakan.
+
+      Contoh penggunaan API:
+      1. **GET** semua pengguna: \`https://skillhub-esdlaboratory.loca.lt/api/users\`
+      2. **GET** semua layanan: \`https://skillhub-esdlaboratory.loca.lt/api/services\`
+      3. **GET** semua portofolio: \`https://skillhub-esdlaboratory.loca.lt/api/portfolio\`
+      `,
     },
     servers: [
       {
-        url: 'http://localhost:3000/api', // Ubah URL untuk pengembangan lokal
-        description: 'Development server',
+        url: "https://skillhub-esdlaboratory.loca.lt/api", // Ubah URL untuk pengembangan lokal
+        description: "Development server",
       },
     ],
     components: {
       securitySchemes: {
         bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
       },
     },
@@ -51,17 +57,17 @@ const swaggerOptions = {
       },
     ],
   },
-  apis: ['./routes/*.js'], // Path to the API docs
+  apis: ["./routes/*.js"], // Path to the API docs
 };
 
 // Generate Swagger docs
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const corsOptions = {
-  origin: '*', // Mengizinkan semua origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: "*", // Mengizinkan semua origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Jika kamu perlu mengirim cookie atau autentikasi berbasis sesi
 };
 
@@ -71,22 +77,22 @@ app.use(express.json());
 app.use(bodyParser.json()); // For parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 app.use(fileUpload());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // API routes
-app.use('/api', userRoutes);
-app.use('/api', portfolioRoutes);
-app.use('/api', skillRoutes);
-app.use('/api', userSkillRoutes);
-app.use('/api', authRoutes);
-app.use('/api', serviceRoutes);
-app.use('/api', orderRoutes);
-app.use('/api', midtransRoutes);
-app.use('/api', RatingRoutes);
-app.use('/api', bankRoutes);
-app.use('/api', bankAccountRoutes);
-app.use('/api', skillSwapRoutes);
-app.use('/api', waRoutes);
+app.use("/api", userRoutes);
+app.use("/api", portfolioRoutes);
+app.use("/api", skillRoutes);
+app.use("/api", userSkillRoutes);
+app.use("/api", authRoutes);
+app.use("/api", serviceRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", midtransRoutes);
+app.use("/api", RatingRoutes);
+app.use("/api", bankRoutes);
+app.use("/api", bankAccountRoutes);
+app.use("/api", skillSwapRoutes);
+app.use("/api", waRoutes);
 
 const port = 3000;
 app.listen(port, () => {

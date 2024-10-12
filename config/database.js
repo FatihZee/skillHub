23e37542c27d4f5d441f41382f1943fa38e2354a
FATIH FIKRY OKTAVIANTO <viananto1234@gmail.com>
2024-10-12 18:50:44 +0700

    Backup Database

--- config/database.js
+++ config/database.js
@@ -1,22 +1,22 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('skillHub_difficulty', 'skillHub_difficulty', '8a11618a53d22819efcd993fed4a6e5cfab4489e', {
-    host: '5xe.h.filess.io',
-    dialect: 'mysql',
-    dialectModule: require('mysql2'),
-    port: 3307,
+const sequelize = new Sequelize("skillhub", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  dialectModule: require("mysql2"),
+  port: 3306,
 });
 
 async function connect() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connected to the database!');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to the database!");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
 
 module.exports = {
-    connect,
-    sequelize,
+  connect,
+  sequelize,
 };
